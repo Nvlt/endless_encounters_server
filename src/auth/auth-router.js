a340b6953135a4d15d4be8347da2f0dc8aa911ae
John <jpending@gmail.com>
2020-10-30 11:03:04 -0400

    insert return for log in

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,50 +1,50 @@
-const express = require('express');
-const AuthService = require('./auth-service');
-const {v4:uuid} = require('uuid');
-const { requireAuth } = require('../middleware/jwt-auth');
-const userServices = require('../user/user-service');
+const express=require('express');
+const AuthService=require('./auth-service');
+const {v4: uuid}=require('uuid');
+const {requireAuth}=require('../middleware/jwt-auth');
+const userServices=require('../user/user-service');
 
-const authRouter = express.Router();
-const jsonBodyParser = express.json();
+const authRouter=express.Router();
+const jsonBodyParser=express.json();
 
 authRouter
   .route('/login')
   .post(jsonBodyParser, async (req, res, next) => {
-    const { username, password } = req.body;
-    console.log(username,password)
-    const loginUser = { username, password };
+    const {username, password}=req.body;
+    console.log(username, password)
+    const loginUser={username, password};
 
-    for (const [key, value] of Object.entries(loginUser)) {
-      if (value == null) {
+    for(const [key, value] of Object.entries(loginUser)) {
+      if(value==null) {
         return res.status(400).json({
           error: `Missing '${key}' in request body`
         });
       }
     }
     try {
-      const dbUser = await AuthService.getUserWithUserName(req.app.get('db'), loginUser.username);
-      if (!dbUser) return res.status(400).json({ error: 'Incorrect username or password' });
+      const dbUser=await AuthService.getUserWithUserName(req.app.get('db'), loginUser.username);
+      if(!dbUser) return res.status(400).json({error: 'Incorrect username or password'});
 
-      const compareMatch = await AuthService.comparePasswords(loginUser.password, dbUser.password);
-      if (!compareMatch) return res.status(400).json({ error: 'Incorrect username or password' });
+      const compareMatch=await AuthService.comparePasswords(loginUser.password, dbUser.password);
+      if(!compareMatch) return res.status(400).json({error: 'Incorrect username or password'});
 
-      const sub = dbUser.username;
-      const payload = {
+      const sub=dbUser.username;
+      const payload={
         user_id: dbUser.id,
         name: dbUser.name,
-        access_token:await userServices.updateAccessToken(req.app.get('db'),sub)
+        access_token: await userServices.updateAccessToken(req.app.get('db'), sub)
       }
-      res.send({
+      return res.send({
         authToken: AuthService.createJwt(sub, payload)
       })
     }
-    catch (error) {
+    catch(error) {
       next(error);
-    }   
+    }
   })
   .put(requireAuth, (req, res) => {
-    const sub = req.user.username
-    const payload = {
+    const sub=req.user.username
+    const payload={
       user_id: req.user.id,
       name: req.user.name
     }
@@ -53,4 +53,4 @@ authRouter
     });
   });
 
-module.exports = authRouter;
+module.exports=authRouter;
--- src/user/user-router.js
+++ src/user/user-router.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const path = require('path');
-const UserService = require('./user-service');
-const {v4:uuid} = require('uuid');
+const express=require('express');
+const path=require('path');
+const UserService=require('./user-service');
+const {v4: uuid}=require('uuid');
 
-const userRouter = express.Router();
-const jsonBodyParser = express.json();
-userRouter.route('/entity/:id').get(jsonBodyParser, async (req,res,next)=>{
-  const db = req.app.get('db');
-  const {id} = req.params;
-  const data = await UserService.getUserGameData(db, id);
+const userRouter=express.Router();
+const jsonBodyParser=express.json();
+userRouter.route('/entity/:id').get(jsonBodyParser, async (req, res, next) => {
+  const db=req.app.get('db');
+  const {id}=req.params;
+  const data=await UserService.getUserGameData(db, id);
   console.log(data);
-  if(!data.length)
-  {
-    return res.status(400).json({Error:"Denied"})
+  if(!data.length) {
+    return res.status(400).json({Error: "Denied"})
   }
   return res.status(200).json(data);
 
@@ -20,10 +19,10 @@ userRouter.route('/entity/:id').get(jsonBodyParser, async (req,res,next)=>{
 userRouter
   .route('/')
   .post(jsonBodyParser, async (req, res, next) => {
-    const { email, password, username } = req.body;
+    const {email, password, username}=req.body;
 
-    for (const field of ['email', 'username', 'password']) {
-      if (!req.body[field]) {
+    for(const field of ['email', 'username', 'password']) {
+      if(!req.body[field]) {
         return res.status(400).json({
           error: `Missing ${field} in request body`
         });
@@ -31,30 +30,30 @@ userRouter
     }
 
     try {
-      const passwordError = UserService.validatePassword(password);
-      if (passwordError) return res.status(400).json({ error: passwordError });
+      const passwordError=UserService.validatePassword(password);
+      if(passwordError) return res.status(400).json({error: passwordError});
 
-      const hasUsername = await UserService.hasUserWithUserName(req.app.get('db'), username);
-      if (hasUsername) return res.status(400).json({ error: 'Username already taken' });
+      const hasUsername=await UserService.hasUserWithUserName(req.app.get('db'), username);
+      if(hasUsername) return res.status(400).json({error: 'Username already taken'});
 
-      const hashedPassword = await UserService.hashPassword(password);
+      const hashedPassword=await UserService.hashPassword(password);
 
-      const newUser = {
+      const newUser={
         email,
         username,
         password: hashedPassword,
-        access_token:uuid()
+        access_token: uuid()
 
       }
 
-      const user = await UserService.insertUser(req.app.get('db'), newUser);
+      const user=await UserService.insertUser(req.app.get('db'), newUser);
 
-      res.status(201)
+      return res.status(201)
         .location(path.posix.join(req.originalUrl, `/${user.id}`))
         .json(UserService.serializeUser(user))
-    } catch (error) {
+    } catch(error) {
       next(error);
     }
   });
 
-  module.exports = userRouter;
+module.exports=userRouter;
