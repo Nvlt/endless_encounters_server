049fbad29bc1aa0e3918cb940881bc519222223e
Tiff Joy <tiffanysummerford@gmail.com>
2020-11-09 13:05:47 -0500

    basics

---
+++ test/story.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const supertest = require('supertest');
+const StoryService = require('../src/story/story-service');
+const app = require('../src/app');
+
+describe('StoryService object', () => {
+
+})
