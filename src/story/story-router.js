15abb3c3e1e0c4fa34f9c82029af5c098cb05fd4
John <jpending@gmail.com>
2020-11-10 16:39:17 -0500

    merge

--- src/story/story-router.js
+++ src/story/story-router.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const path = require('path');
-let storyService = require('./story-service');
-const router = express.Router();
-const abilities = require('../../gameLogic/Content/abilities');
-const jsonBodyParser = express.json();
+const express=require('express');
+const path=require('path');
+let storyService=require('./story-service');
+const router=express.Router();
+const abilities=require('../../gameLogic/Content/abilities');
+const jsonBodyParser=express.json();
 
-storyService = new storyService();
-
-
-router.route('/:id').get(jsonBodyParser, async (req, res, next) => {
-      const db= req.app.get('db');
-      const {id} = req.params;
-      const data = await storyService.getStoryByIDForEngine(db,id);
-      
-      res.json(data);
-
-  });
+storyService=new storyService();
 
 
-  module.exports = router;
+router
+    .route('/:id')
+    .get(jsonBodyParser, async (req, res, next) => {
+        const db=req.app.get('db');
+        const {id}=req.params;
+        const data=await storyService.getStoryByIDForEngine(db, id);
+
+        res.json(data);
+
+    });
+
+
+module.exports=router;
