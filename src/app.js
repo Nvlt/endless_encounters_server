6288f892c69117bfc82b1268e2b7c8bd3088f23b
John <jpending@gmail.com>
2020-11-10 17:02:52 -0500

    merge

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express=require('express');
-const morgan=require('morgan');
 const cors=require('cors');
+const app=express();
+const morgan=require('morgan');
 const helmet=require('helmet');
 const {NODE_ENV}=require('./config');
 const errorHandler=require('./middleware/error-handler');
@@ -11,14 +12,13 @@ const entityRouter=require('./entity/entity-router');
 const storyRouter=require('./story/story-router');
 //const inventoryRouter = require('./inventory/inventory-router');
 
-const app=express();
+
 
 app.use(morgan((NODE_ENV==='production')? 'tiny':'common', {
   skip: () => NODE_ENV==='test'
 }));
 app.use(cors());
 app.use(helmet());
-app.options('*', cors())
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/choice', choiceRouter);
--- src/entity/entity-router.js
+++ src/entity/entity-router.js
@@ -1,19 +1,23 @@
 const express=require('express');
 const path=require('path');
 let entityService=require('./entity-service');
-entityService = new entityService();
+entityService=new entityService();
 const router=express.Router();
 const jsonBodyParser=express.json();
 
-router.route('/:id').get(jsonBodyParser, async (req, res, next) => {
-    const {id}=req.params;
-    const db=req.app.get('db')
-    if(!typeof id=='number') {
-        return res.status(400).json({Error: 'Not found.'})
-    }
-    const data=await entityService.getEntityByIdForEngine(db, id);
-    res.status(200).json(data);
+router
+    .route('/:id')
+    .get(jsonBodyParser, async (req, res, next) => {
+        const {id}=req.params;
+        const db=req.app.get('db')
+        if(!typeof id=='number') {
+            return res
+                .status(400)
+                .json({Error: 'Not found.'})
+        }
+        const data=await entityService.getEntityByIdForEngine(db, id);
+        res.status(200).json(data);
 
-});
+    });
 
 module.exports=router;
